967338cd1e13ad94880e57dfe9b5eca2b19a1eb2
neilkplugins <neilk@devmini.meadowbarn>
2020-06-04 09:28:34 +0100

    Added API support and made MQTT configurable

--- GlowmarktCAD.indigoPlugin/Contents/Server Plugin/plugin.py
+++ GlowmarktCAD.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -12,12 +12,99 @@ import indigo
 import requests
 import json
 import time
-from datetime import datetime
+from datetime import datetime, timedelta, date
 
 
 ################################################################################
 # Globals
 ################################################################################
+state_list = ["From-00-00","From-00-30","From-01-00","From-01-30","From-02-00","From-02-30","From-03-00","From-03-30","From-04-00","From-04-30","From-05-00","From-05-30","From-06-00","From-06-30","From-07-00","From-07-30","From-08-00","From-08-30","From-09-00","From-09-30","From-10-00","From-10-30","From-11-00","From-11-30","From-12-00","From-12-30","From-13-00","From-13-30","From-14-00","From-14-30","From-15-00","From-15-30","From-16-00","From-16-30","From-17-00","From-17-30","From-18-00","From-18-30","From-19-00","From-19-30","From-20-00","From-20-30","From-21-00","From-21-30","From-22-00","From-22-30","From-23-00","From-23-30"]
+
+
+############################
+# API Functions
+#############################
+
+def token_check_valid(self):
+	time_now = datetime.now() + timedelta(hours=1)
+	expiry_time = datetime.fromtimestamp(self.pluginPrefs['token_expires'])
+	if expiry_time > time_now:
+		self.debugLog("Time remaining on token is " + str(expiry_time - time_now))
+		return True
+	else:
+		self.debugLog("Get new Token - One hour or less remaining valid")
+		return False
+
+
+def refresh_token(self):
+	url = "https://api.glowmarkt.com/api/v0-1/auth"
+
+	payload = "{\n\"username\": \"" + self.pluginPrefs['bright_account'] + "\",\n\"password\": \"" + \
+			  indigo.pluginPrefs['bright_password'] + "\"\n}"
+	headers = {
+		'Content-Type': 'application/json',
+		'applicationId': "b0f1b774-a586-4f72-9edd-27ead8aa7a8d",
+	}
+	try:
+		response = requests.request("POST", url, headers=headers, data=payload)
+		response.raise_for_status()
+	except requests.exceptions.HTTPError as err:
+		self.debugLog("HTTP Error when refreshing token")
+	except Exception as err:
+		self.debugLog("Other error when refreshing token")
+
+	self.debugLog(response)
+	response_json = response.json()
+	self.debugLog(response_json)
+	self.debugLog(response_json['token'])
+	if response.status_code != 200:
+		self.errorLog("Failed to Authenticate with Glow Servers, Check Password and Account Name")
+		errorsDict = indigo.Dict()
+		errorsDict['bright_password'] = "Failed to Authenticate with Glow Servers, Check Password and Account Name"
+		return False
+	else:
+		self.pluginPrefs['token'] = response_json['token']
+		self.pluginPrefs['token_expires'] = response_json['exp']
+		self.debugLog("Token is " + self.pluginPrefs['token'])
+		self.debugLog("Expiry is " + str(self.pluginPrefs['token_expires']))
+		return True
+
+
+def get_resources(self):
+	if not token_check_valid(self):
+		refresh_token(self)
+	url = "https://api.glowmarkt.com/api/v0-1/resource"
+
+	payload = {}
+	headers = {
+		'Content-Type': 'application/json',
+		'applicationId': 'b0f1b774-a586-4f72-9edd-27ead8aa7a8d',
+	}
+	headers['token'] = self.pluginPrefs['token']
+	self.debugLog("Getting Resources")
+	try:
+		response = requests.request("GET", url, headers=headers, data=payload)
+		response.raise_for_status()
+	except requests.exceptions.HTTPError as err:
+		self.debugLog("HTTP Error when collecting resources")
+	except Exception as err:
+		self.debugLog("Other error when collecting resources")
+	self.debugLog(response.text.encode('utf8'))
+	response_json = response.json()
+	resource_list=[]
+	if response.status_code == 200:
+		for resources in response_json:
+			self.debugLog(resources['classifier'])
+			resource_list.append(resources['classifier'])
+			self.pluginPrefs[resources['classifier']] = resources['resourceId']
+			self.debugLog(self.pluginPrefs[resources['classifier']])
+		self.pluginPrefs['resource_list']=resource_list
+		return True
+	else:
+		self.debugLog("Error getting resources")
+		return False
+
+
 
 
 ################################################################################
@@ -70,12 +157,12 @@ class Plugin(indigo.PluginBase):
 	def update(self, device):
 		# device.stateListOrDisplayStateIdChanged()
 		mqttPlugin = indigo.server.getPlugin("com.flyingdiver.indigoplugin.mqtt")
-		if mqttPlugin.isEnabled() and self.pluginPrefs['MQTT_enable']:
+		if mqttPlugin.isEnabled() and self.pluginPrefs['MQTT_enable']and device.deviceTypeId =="GlowmarktCAD":
 			props = {
 				'message_type': "##GlowmarktCAD##"
 			}
 			while True:
-				message_data = mqttPlugin.executeAction("fetchQueuedMessage", deviceId=634573472, props=props,
+				message_data = mqttPlugin.executeAction("fetchQueuedMessage", deviceId=int(device.pluginProps["brokerID"]), props=props,
 														waitUntilDone=True)
 				if message_data == None:
 					return
@@ -122,13 +209,15 @@ class Plugin(indigo.PluginBase):
 					self.debugLog("Failed to complete updates for Glow device " + device.name)
 					self.debugLog(e)
 		else:
-			self.debugLog("MQTT Connector not enabled or MQTT option not configured in Plugin Config - Skipping Message Queue Check")
+			self.debugLog("MQTT Connector not enabled or MQTT option not configured in Plugin Config - Skipping Message Queue Check or not an MQTT device")
 		return
 
 	########################################
 	# UI Validate, Device Config
 	########################################
 	def validateDeviceConfigUi(self, valuesDict, typeId, device):
+		if typeId == "GlowmarktElec_kWh":
+			return (True, valuesDict)
 		self.debugLog(valuesDict)
 		if valuesDict['brokerID'] == "":
 			self.errorLog("MQTT Broker Device cannot be empty")
@@ -166,8 +255,13 @@ class Plugin(indigo.PluginBase):
 				'applicationId': "b0f1b774-a586-4f72-9edd-27ead8aa7a8d",
 				'Content-Type': 'application/json'
 			}
-
-			response = requests.request("POST", url, headers=headers, data = payload)
+			try:
+				response = requests.request("POST", url, headers=headers, data=payload)
+				response.raise_for_status()
+			except requests.exceptions.HTTPError as err:
+				self.debugLog("HTTP Error when authenticating to Glowmarkt")
+			except Exception as err:
+				self.debugLog("Other error when authenticating to Glowmarkt")
 			self.debugLog(response)
 			response_json = response.json()
 			self.debugLog(response_json)
@@ -182,6 +276,8 @@ class Plugin(indigo.PluginBase):
 				self.pluginPrefs['token_expires']=response_json['exp']
 				self.debugLog("Token is "+ self.pluginPrefs['token'])
 				self.debugLog("Expiry is "+ str(self.pluginPrefs['token_expires']))
+				get_resources(self)
+
 		except:
 			self.debugLog("Unknown error connecting to Glowmarkt")
 			errorsDict = indigo.Dict()
@@ -221,45 +317,48 @@ class Plugin(indigo.PluginBase):
 		retList.sort(key=lambda tup: tup[1])
 		return retList
 
-	############################
-	# API Functions
-	#############################
+	def resourceListGenerator(self, filter="", valuesDict=None, typeId="", targetId=0):
+		resource_list = []
+		for resource in self.pluginPrefs['resource_list']:
+			self.debugLog(resource)
+			resource_list.append((resource,resource))
+		return resource_list
 
-	def token_check_valid(self):
-		time_now = datetime.now() + timedelta(hours=1)
-		expiry_time=datetime.fromtimestamp(indigo.pluginPrefs['token_expires'])
-		if expiry_time > time_now:
-			self.debugLog("Time remaining on token is "+str(expiry_time - time_now))
-			return True
-		else:
-			self.debugLog("Get new Token - One hour or less remaining valid")
-			return False
 
-	def refresh_token(self):
-		url = "https://api.glowmarkt.com/api/v0-1/auth"
+	def refresh_daily_electricity(self, pluginAction, device):
+		today = str(date.today())
+		resource_type = device.pluginProps['resource_type']
+		self.debugLog(resource_type)
+		resource = self.pluginPrefs[resource_type]
+		url = "https://api.glowmarkt.com/api/v0-1/resource/"+resource+"/readings?from="+today+"T00:00:00&to="+today+"T23:59:00&function=sum&period=PT30M"
 
-		payload = "{\n\"username\": \"" + indigo.pluginPrefs['bright_account'] + "\",\n\"password\": \"" + indigo.pluginPrefs['bright_password'] + "\"\n}"
+		payload = {}
 		headers = {
 			'Content-Type': 'application/json',
-			'applicationId': "b0f1b774-a586-4f72-9edd-27ead8aa7a8d",
-			'Content-Type': 'application/json'
+			'applicationId': 'b0f1b774-a586-4f72-9edd-27ead8aa7a8d'
 		}
-		response = requests.request("POST", url, headers=headers, data=payload)
-		self.debugLog(response)
+		headers['token']=self.pluginPrefs['token']
+		try:
+			response = requests.get(url, headers=headers, data=payload)
+			response.raise_for_status()
+		except requests.exceptions.HTTPError as err:
+			self.debugLog("HTTP Error updating 30 min elec rates")
+		except Exception as err:
+			self.debugLog("Other error 30 min elec")
+
+
 		response_json = response.json()
-		self.debugLog(response_json)
-		self.debugLog(response_json['token'])
-		if response.status_code != 200:
-			self.errorLog("Failed to Authenticate with Glow Servers, Check Password and Account Name")
-			errorsDict = indigo.Dict()
-			errorsDict['bright_password'] = "Failed to Authenticate with Glow Servers, Check Password and Account Name"
-			return (False, valuesDict, errorsDict)
-		else:
-			self.pluginPrefs['token'] = response_json['token']
-			self.pluginPrefs['token_expires'] = response_json['exp']
-			self.debugLog("Token is " + self.pluginPrefs['token'])
-			self.debugLog("Expiry is " + str(self.pluginPrefs['token_expires']))
+
+		device_states = []
+		state_count = 0
+		for rates in response_json['data']:
+			device_states.append({ 'key': state_list[state_count] , 'value' : rates[1] , 'decimalPlaces' : 4})
+
+			self.debugLog(state_list[state_count]+" "+str(rates[1]))
+			state_count += 1
+		device.updateStatesOnServer(device_states)
 
 
-	return
+
+
 
